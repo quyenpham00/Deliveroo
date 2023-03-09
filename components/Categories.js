e05caee5cdf3cb8c61e4756425ce8659e1586bf9
tien_nguyen_ops <tien_nguyen@datahouse.com>
2023-03-10 00:14:26 +0700

    10032023: Pulling Data from Sanity Studio

--- components/Categories.js
+++ components/Categories.js
@@ -1,22 +1,43 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CategoryCard from './CategoryCard'
+import sanityClient from '../sanity';
 
 const Categories = () => {
-  return (
-    <ScrollView
-      contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-    >
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
-      <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
-    </ScrollView>
-  )
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		sanityClient.fetch(
+			`
+				*[_type == "category"]
+			`
+		).then((data) => {
+			setCategories(data);
+		})
+	}, []);
+
+	return (
+		<ScrollView
+		contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
+		horizontal
+		showsHorizontalScrollIndicator={false}
+		>
+
+		{categories?.map((category) => {
+			<CategoryCard 
+				key={category._id}
+				imgUrl= {urlFor(category.image).width(200).url()}
+				title= {category.name}
+			/>
+		})}
+		{/* <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
+		<CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
+		<CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
+		<CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
+		<CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' />
+		<CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing' /> */}
+		</ScrollView>
+	)
 }
 
 export default Categories
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
   TextInput,
   ScrollView,
 } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
   ChevronDownIcon,
@@ -16,15 +16,35 @@ import {
 } from 'react-native-heroicons/outline'
 import Categories from '../components/Categories'
 import FeaturedRow from '../components/FeaturedRow'
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation()
-
+  const [featuredCatetories, setFeaturedCategories] = useState([]);
+  
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShow: false,
     })
   }, [])
+
+  //Tien Code
+  useEffect(() => {
+	sanityClient.fetch(
+		`
+			*[_type == 'featured] {
+				...,
+			restaurants[]->{
+				...,
+				dishes[]->
+			}
+			}
+		`
+	)
+	.then((data) => {
+		setFeaturedCategories(data);
+	})
+  }, []);
 
   return (
     <SafeAreaView className='bg-white pt-5'>
@@ -61,12 +81,18 @@ const HomeScreen = () => {
         contentContainerStyle={{ paddingBottom: 100 }}
       >
         <Categories />
-        <FeaturedRow
-          id='123'
-          title='Featured'
-          description='Paid placements from our partners'
-        />
-        <FeaturedRow
+
+		{/* Featured */}
+		{featuredCatetories?.map((category) => {
+			<FeaturedRow
+				key={category._id}
+				id={category._id}
+				title={category.name}
+				description={category.short_description}
+			/>
+		})}
+        
+        {/* <FeaturedRow
           id='123'
           title='Tasty Discounts'
           description="Everyone's been enjoying these juicy discounts!"
@@ -75,7 +101,7 @@ const HomeScreen = () => {
           id='123'
           title='Offers near you!'
           description='Why not support your local restaurant tonight!'
-        />
+        /> */}
       </ScrollView>
     </SafeAreaView>
   )
