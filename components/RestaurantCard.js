8a26a731029780380f5fc003b6cc8d68ca0bc606
quyenpham00 <85977597+quyenpham00@users.noreply.github.com>
2023-03-12 15:55:11 +0700

    init screens

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { MapPinIcon, StarIcon } from 'react-native-heroicons/outline'
 import { urlFor } from '../sanity'
+import { useNavigation } from '@react-navigation/native'
 
 const RestaurantCard = ({
   id,
@@ -15,8 +16,25 @@ const RestaurantCard = ({
   long,
   lat,
 }) => {
+  const navigation = useNavigation()
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('Restaurant', {
+          id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          short_description,
+          dishes,
+          long,
+          lat,
+        })
+      }
+    >
       <Image
         source={{ uri: urlFor(imgUrl).url() }}
         className='h-36 w-36 rounded-sm'
